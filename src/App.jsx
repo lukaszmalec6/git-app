203115fff25b1dd07f6c73e031790e43e503cb75
Lukasz Malec <lukaszmalec6@gmail.com>
2017-11-29 11:03:52 +0100

    sass config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import "./sass/main.scss";
 import React, { Component } from "react";
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="intro-component">
         <h1>Hello World</h1>
       </div>
     );
